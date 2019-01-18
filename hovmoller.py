26bfb2ec72acc2a64b8f519e54080a0ff94d7c4b
mikejwx <mcjohnston2803@gmail.com>
2019-01-18 13:30:37 +0000

    Edited rainfall to get the correct(?) rainfall totals in mm, rather than just the integrated rainwater mixing ratio. Also started working on hovmoller.py which is an attempt to synthesize the time evolution of the cloud trail.

---
+++ hovmoller.py
@@ -0,0 +1,9 @@
+"""
+We are going to do a hovmoller diagram of different variables at different levels
+along the length of the cloud trail
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+
+
