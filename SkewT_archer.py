9fcbbe0bc74ac3138a13a46a02fadf38986c7578
mikejwx <30926318+mikejwx@users.noreply.github.com>
2019-01-19 16:24:02 +0000

    Updated the doc strings and made all constants consistent.
    Made sure that the constants used by all the same functions are the same, i.e. cp = cpd = CPD = CP. and made sure that they have the same value i.e. cpd = 1005. J kg^-1 K^-1 everywhere.
    
    Made sure the doc strings and comments are consistent in style across all of the functions. Also made sure that they were correct, and updated them with any of the consistency changes noted above.

--- SkewT_archer.py
+++ SkewT_archer.py
@@ -4,10 +4,18 @@ from matplotlib import gridspec
 from scipy import interpolate
 
 def getQ(TIN, RHIN, PIN):
-    "TIN: input temperatures (deg C)"
-    "RHIN: input relative humidities (%)"
-    "PIN: input pressures (hPa)"
-    "breaks at temps much lower than -80 C,I assume q = 0 here"
+    """
+    Uses equations from Bolton (1980) to calculate the specific humidity from the temperature (TIN), 
+    the relative humidity (RHIN), and the pressure (PIN).
+    
+    TIN: input temperatures (deg C)
+    RHIN: input relative humidities (%)
+    PIN: input pressures (hPa)
+    
+    This method is not appropriate for temperatures lower than -80 C.
+    Specific humidity is set to zero where temperature is lower than -80 C.
+    """
+    
     if (type(TIN) == int) or (type(TIN) in [float, np.float16, np.float32, np.float64]):
         TIN = np.array([TIN])
     if (type(RHIN) == int) or (type(RHIN) in [float, np.float16, np.float32, np.float64]):
@@ -22,8 +30,9 @@ def getQ(TIN, RHIN, PIN):
         p = PIN[:]*100.
     else:
         p = PIN[:]*1.
-    #step one - calculate es    
-    #set-up the constants
+    
+    # Step one - calculate saturation vapour pressure (es)    
+    # Define some coefficients
     c0=0.6105851e+03
     c1=0.4440316e+02
     c2=0.1430341e+01
@@ -33,18 +42,14 @@ def getQ(TIN, RHIN, PIN):
     c6=0.6936113e-08
     c7=0.2564861e-11
     c8=-.3704404e-13
-    #calculate the saturation vapor pressure
+    # Calculate the saturation vapor pressure
     es = c0 + TIN*(c1 + TIN*(c2 + TIN*(c3 + TIN*(c4 + TIN*(c5 + TIN*(c6 + TIN*(c7 + TIN*c8)))))))
     
-    #step-two calculate the actual vapor pressure
+    # Step two - calculate the actual vapor pressure
     ev = RHIN*es/100. #in hPa
     
-    #step three calculate the specific humidity
-    #more constants
-    RV = 461.5
-    RD = 287.05
-    E = RD/RV # epsilon
-    q = ev*E/(p-ev*(1-E)) #kg kg-1
+    # Step three - calculate the specific humidity
+    q = ev*EPS/(p-ev*(1-EPS)) # kg kg-1
     
     i = np.where(TIN < -80.)
     q[i] = 0.
@@ -52,37 +57,50 @@ def getQ(TIN, RHIN, PIN):
     return q
 
 def getGM(TIN, PIN):
+    """
+    Calculate the moist adiabatic lapse rate, GM, for a given temperature and pressure.
+    Assumes that the air is saturated at the given temperature and that all moisture is
+    condensed and removed immediately from the parcel.
+    TIN: Input temperature (deg C)
+    PIN: Input pressure (hPa)
+    """
+    # Ensure the correct units
     if (TIN-273.15) < 0:
         T_C = TIN*1.
     else:
         T_C = TIN - 273.15
+    
+    # Calculate the specific humidity at saturation
     QIN  = getQ(T_C, 100., PIN)[0]
-    g    = 9.81 #gravity
-    Lv   = 2.5e06 #enthalpy of vaporisation
-    R    = 287. #specific gas constant for dry air
-    E    = 0.622
-    cpd  = 1004.
     
-    A = (Lv*QIN)/(R*TIN)
-    B = (E*QIN*Lv**2)/(R*TIN**2)
-    
+    # Calculate the moist adiabatic lapse rate
+    A = (Lv*QIN)/(Rd*TIN)
+    B = (EPS*QIN*Lv**2)/(Rd*TIN**2)
     G_m = g*(1 + A)/(cpd + B)
     
     return G_m
 
 def getDew(QIN, PIN1):
-    "converts the specific humidity to a vapour pressure"
-    "Uses the Bolton equation to calculate the dew point from e"
+    """
+    Convert the specific humidity into a dew point temperature given a pressure.
+    Convert the specific humidity to mixing ratio, then mixing ratio to vapour pressure,
+    Finally, inverts the Clausius-Clapeyron Equation to get the dew point temperature.
+    
+    QIN: Specific humidity (kg kg^-1)
+    PIN1: Pressure (hPa)
+    """
+    
+    # Convert pressure from hPa to Pa
     PIN = PIN1*100.
-    R = 8.314
-    Rd = R/0.0289
-    Rv = R/0.018
-    Lv = 2.501e06
-    E = Rd/Rv
+    
+    # Convert the specific humidity to a mixing ratio
     W = QIN/(1. - QIN)
     es0 = 611.2
     
-    e = W*PIN/(E + W)
+    # Convert the mixing ratio to a vapour pressure
+    e = W*PIN/(EPS + W)
+    
+    # Calculate the dew point temperature from the vapour pressure
     Td = ((1./273.15) - (Rv/Lv)*np.log(e/es0))**(-1.)
     
     return Td
@@ -104,9 +122,6 @@ def getCAPE(TIN1, QIN, PIN, parcel_type = 0):
         Te = TIN1*1.
     
     # Locally define some constants
-    g = 9.81 # gravity
-    cpd = 1005. # specific heat capacity of dry air
-    R = 287.05 # gas constant for dry air
     G_d = g/cpd # dry adiabatic lapse rate
     
     ### Parcel Type
@@ -117,7 +132,7 @@ def getCAPE(TIN1, QIN, PIN, parcel_type = 0):
         T_d0 = getDew(Q_0, PIN[0]) # get the surface dew point
     elif parcel_type == 1:
         # mean over lowest ~ 500 m
-        rho = PIN[0]/(R*Te[0])
+        rho = PIN[0]/(Rd*Te[0])
         dp500 = 500.*rho*g/100.
         p_dummy = PIN[0] - np.arange(0., dp500+0.1)
         Tp = PTtoTemp(np.mean(interpolate.interp1d(PIN, temp2theta(Te, PIN))(p_dummy)), PIN[0])
@@ -147,7 +162,7 @@ def getCAPE(TIN1, QIN, PIN, parcel_type = 0):
         for level in range(1,len(Te)):
             p_level_half = 0.5*(PIN[level] + PIN[level-1])
             # convert the thickness of the layer from pressure to height units
-            rho = (p_level_half*100.)/(R*Tp)
+            rho = (p_level_half*100.)/(Rd*Tp)
             dz = - (PIN[level] - PIN[level-1])*100./(rho*g) # dp/rho*g, rho = p/RT
 
             if PIN[level] != 0.0:
@@ -203,7 +218,7 @@ def getCAPE(TIN1, QIN, PIN, parcel_type = 0):
         for level in range(1,len(Te)):
             p_level_half = 0.5*(PIN[level] + PIN[level-1])
             # convert the thickness of the layer from pressure to height units
-            rho = (p_level_half*100.)/(R*Tp)
+            rho = (p_level_half*100.)/(Rd*Tp)
             dz = - (PIN[level] - PIN[level-1])*100./(rho*g) # dp/rho*g, rho = p/RT
             
             if PIN[level] != 0.0:
@@ -225,17 +240,23 @@ def getCAPE(TIN1, QIN, PIN, parcel_type = 0):
     #print LFCp
     return CAPE, CIN, Tps, LCLp, LFCp
 
-def PTtoTemp(TIN, PIN):
-    "converts potential temperature to temperature"
-    Rd = 287.05
-    cp = 1005.
-    Temperature = TIN/((1000./PIN)**(Rd/cp))
+def PTtoTemp(theta, PIN):
+    """
+    Converts potential temperature to temperature.
+    theta: potential temperature (K)
+    PIN: pressure (hPa)
+    """
+    
+    Temperature = theta/((1000./PIN)**(Rd/cpd))
     
     return Temperature
 
 def temp2theta(TIN, PIN, P0 = 1000.):
-    "TIN: input temperatures (K)"
-    "PIN: input pressures (hPa)"
+    """
+    Converts temperature to potential temperature
+    TIN: input temperature (K)
+    PIN: input pressures (hPa)
+    """
     
     # check that a temperature is passed in Kelvin.
     if type(TIN) == np.ndarray:
@@ -244,24 +265,20 @@ def temp2theta(TIN, PIN, P0 = 1000.):
     elif TIN - 273.15 < -100:
         TIN += 273.15
     
-    R = 287. #gas constant for dry air
-    cp = 1004. #specific heat capacity for dry air
-    theta = TIN*(P0/PIN)**(R/cp)
+    theta = TIN*(P0/PIN)**(Rd/cpd)
+    
     return theta
 
 def getLFC(Tp, Te, QIN, PIN):
     """
-    Function to calculate the LFC.
+    Function to calculate the level of free convection LFC.
     Tp = the temperature of a parcel, K
     Te = the temperature of the environment, K
     QIN = the specific humidity profile, kg/kg
     PIN = the pressure profile, hPa
     """
-    # Locally define some constants
-    g    = 9.81 # gravity
-    R    = 287.05 # specific gas constant for dry air
-    cpd  = 1005. # specific heat capacity of dry air at constant pressure
-    G_d  = g/cpd # dry adiabatic lapse rate
+    # Locally define dry adiabatic lapse rate
+    G_d  = g/cpd
     
     # Lets get some unit conversions going
     Q_0  = np.mean(QIN[0]) # surface specific humidity
@@ -306,11 +323,10 @@ def getLFC(Tp, Te, QIN, PIN):
         LFCT = None
     
     return LFCT
-
-
-#contains methods to plot a skew T when passed temperature and specific humidity values
-#varying with height and pressure
-#data at altitudes lower than the 100hPa level is plotted
+################################################################################
+# Methods to plot a skew T when passed temperature and specific humidity       #
+# values varying with height and pressure                                      #
+################################################################################
 
 ### Constants
 g = 9.81 #gravity
@@ -320,7 +336,7 @@ R = 8.314 #gas constant
 Mrd = 0.0289 #molar mass of dry air
 Mrv = 0.018 #molar mass of water vapour
 EPS = Mrv/Mrd #ratio of molar masses of water vapour and dry air
-Rd = R/Mrd #gas constant for dry air
+Rd = 287.05 #gas constant for dry air
 Rv = R/Mrv #gas constant for water vapour
 Lv = 2.501e06 #latent heat of vapourisation
 Lm = 3.3e05 #latent heat of melting
@@ -328,33 +344,38 @@ Ls = Lv + Lm #latent heat of sublimation
 gamma_d = -g/cpd #dry adiabatic lapse rate
 p0 = 1000. #standard pressure level
 adiabat_p = np.arange(1050., 99., -5.) #pressure levels to calculate adiabats
-w_p = np.arange(1050., 500., -5.) # pressure level on which to plot lines of constant w
-target_w = np.array([0.1, 1, 2, 4, 7, 10, 15, 20, 30])*1e-3 # constant mixing ratios to plot lines
-T0 = np.arange(-40, 300., 20.)
-#p0 level temperatures to calculate dry adiabats
+q_p = np.arange(1050., 500., -5.) # pressure levels on which to plot lines of constant specific humidity (q)
+target_q = np.array([0.1, 1, 2, 4, 7, 10, 15, 20, 30])*1e-3 # constant specific humidities to plot lines
+T0 = np.arange(-40, 300., 20.) # p0 level temperatures to calculate dry adiabats
 T1 = np.arange(-40., 90.1, 5)
 
 def dry_adiabats():
-	#uses T0 to draw dry adiabats along
-	#uses pressure levels every 5hPa and poisson equation for potential temperatue
-	adiabats = np.zeros((len(adiabat_p), len(T0)))
+    """
+	Uses T0 to draw dry adiabats. Uses pressure levels every 5hPa and 
+    poisson equation for potential temperature
+	"""
+    adiabats = np.zeros((len(adiabat_p), len(T0)))
 	for T in range(len(T0)):
 		adiabats[:,T] = (T0[T]+273.15)/(p0/adiabat_p)**(Rd/cpd) - 273.15
 			
 	return adiabats
 
 def get_saturation_mixing_ratio(temp):
-	#calculates the mixing ratio
-	#requires temperature in deg C
+    """
+	Calculate the saturation mixing ratio for a given temperature (temp).
+    temp: input temperature (deg C)
+    """
 	es = 6.112*np.exp(17.67*temp/(temp+243.5))
-	rs = EPS*es/(adiabat_p - es)
+	ws = EPS*es/(adiabat_p - es)
 	
-	return rs
+	return ws
 
 def moist_adiabats():
-    #uses T1 to draw moist adiabats along
-    #uses pressure levels every 5hPa to calculate an estimate of the equivalent potential temperature
-    #using the moist adiabatic lapse rate
+    """
+    Draws the moist adiabats for the skew T.
+    Uses T1, temperatures in deg C, at 1000 hPa to draw these adiabats.
+    Also uses pressure every 5 hPa. 
+    """
     moist_adiabats = np.zeros((len(adiabat_p), len(T1)))
     i_1000 = np.where(adiabat_p == 1000)[0][0]
     moist_adiabats[i_1000,:] = T1
@@ -375,37 +396,50 @@ def moist_adiabats():
 
     return moist_adiabats
 
-def const_ws():
+def const_qs():
     """
-    Creates curves for lines with constant specific humidity
+    Creates curves for lines of constant specific humidity
     """
-    const_ws = np.zeros((len(w_p), len(target_w)))
-    for iw in range(len(target_w)):
-        const_ws[:,iw] = getDew(target_w[iw]/(1. + target_w[iw]), w_p)-273.15
+    const_qs = np.zeros((len(q_p), len(target_q)))
+    for iq in range(len(target_q)):
+        const_qs[:,iq] = getDew(target_q[iq]/(1. + target_q[iq]), q_p)-273.15
     
-    return const_ws
+    return const_qs
 
 wmo_p = [1000, 925, 850, 700, 500, 400, 300, 250, 200, 150, 100]
 
 def skew(temperature, pressure):
-    #skews the temperatures by Gamma_d/2
+    """
+    Creates a skew for an input temperature.
+    The temperature could represent, actual air temperature, dry adiabats, moist adiabats,
+    lines of constant specific humidity, actual specific humidity...
+    The amount of skew is designed to roughly match the amount of skew on UWyoming's skewT plots
+    """
+    
     c = 0.01219513#np.tan(30*np.pi/180.)
     skewedT = np.zeros_like(temperature) + temperature
     for p_i in range(len(pressure)):
         dp = np.log10(pressure[p_i]/1000.)
         skewedT[p_i] -= dp/c
+    
     return skewedT
 
 def paper():
+    """
+    Plots the paper used to plot skew T diagrams on without requiring input data.
+    Additional plots of skewed temperatures can be overlaid.
+    It might be possible to include this is a subplot and wind barbs in a separate subplot
+    using windBarbs().
+    """
     theta = dry_adiabats()
     thetae = moist_adiabats()
-    my_w = const_ws()
+    my_q = const_qs()
     for sfc in range(len(T0)):
         theta[:,sfc] = skew(theta[:,sfc], adiabat_p)
     for sfc in range(len(T1)):
         thetae[:,sfc] = skew(thetae[:,sfc], adiabat_p)
     for sfc in range(len(target_w)):
-        my_w[:,sfc] = skew(my_w[:,sfc], w_p)
+        my_q[:,sfc] = skew(my_q[:,sfc], q_p)
     
     therms = np.arange(-120., 40.1, 5.)
     isotherms = np.zeros((len(adiabat_p), len(therms)))
@@ -419,19 +453,21 @@ def paper():
     plt.semilogy(theta, adiabat_p, color = 'g', lw = 0.25)
     plt.semilogy(thetae, adiabat_p, color = 'b', lw = 0.25)
     plt.semilogy(isotherms, adiabat_p, color = 'gray', lw = 0.25, ls = '--')
-    plt.semilogy(my_w, w_p, color = 'purple', lw = 0.5, ls = '--')
-    w_labels = [w*1000. if w*1000 < 1 else int(w*1000) for w in target_w]
-    for iw in range(len(target_w)):
-        plt.text(my_w[np.where(w_p == 850)[0][0], iw], 850., str(w_labels[iw]), color = 'purple')
+    plt.semilogy(my_q, q_p, color = 'purple', lw = 0.5, ls = '--')
+    q_labels = [q*1000. if q*1000 < 1 else int(q*1000) for q in target_q]
+    for iq in range(len(target_q)):
+        plt.text(my_q[np.where(q_p == 850.)[0][0], iq], 850., str(q_labels[iq]), color = 'purple')
     plt.gca().invert_yaxis()
     plt.ylim([1050, 100])
     plt.gca().yaxis.grid()
     plt.yticks(wmo_p, wmo_p)
     plt.xlim([-40, 40])
-#paper()
 
 def windBarbs(u_in, v_in, p_in, gs):
-    #I want to only use winds on the wmo levels
+    """
+    Plots wind barbs for given u and v wind components at wmo pressure levels.
+    """
+    # Only use winds on the wmo levels
     u_wmo = []
     v_wmo = []
     for i in range(len(p_in)):
@@ -440,11 +476,11 @@ def windBarbs(u_in, v_in, p_in, gs):
             v_wmo.append(v_in[i])
     u_wmo = np.array(u_wmo)
     v_wmo = np.array(v_wmo)
-    #adds wind barbs to the SkewT
+    # Adds wind barbs to the SkewT
     ax2 = plt.subplot(gs[1])
     ax2.barbs(np.zeros_like(wmo_p), wmo_p, u_wmo, v_wmo, barbcolor = 'k', clip_on=False, zorder=100)
     plt.xlim([-1, 1])
-    plt.ylim([1005, 100])
+    plt.ylim([1005., 100.])
     plt.yticks([])
     plt.xticks([])
     plt.axis('off')
@@ -483,14 +519,14 @@ def plotSkewT(temp, t_dew, p, u = np.array([-999]), v = np.array([-999]),
     ## plot the skew T paper ##
     theta = dry_adiabats()
     thetae = moist_adiabats()
-    my_w = const_ws()
+    my_q = const_qs()
     # skew the adiabats and the mixing ratio lines
     for sfc in range(len(T0)):
         theta[:,sfc] = skew(theta[:,sfc], adiabat_p)
     for sfc in range(len(T1)):
         thetae[:,sfc] = skew(thetae[:,sfc], adiabat_p)
-    for sfc in range(len(target_w)):
-        my_w[:,sfc] = skew(my_w[:,sfc], w_p)
+    for sfc in range(len(target_q)):
+        my_q[:,sfc] = skew(my_q[:,sfc], q_p)
     # get some isotherms
     therms = np.arange(-120., 40.1, 5.)
     isotherms = np.zeros((len(adiabat_p), len(therms)))
@@ -509,12 +545,12 @@ def plotSkewT(temp, t_dew, p, u = np.array([-999]), v = np.array([-999]),
     ax.semilogy(thetae, adiabat_p, color = 'b', lw = 0.25)
     # isotherms
     ax.semilogy(isotherms, adiabat_p, color = 'gray', lw = 0.25, ls = '--')
-    # mixing ratios
-    ax.semilogy(my_w, w_p, color = 'purple', lw = 0.5, ls = '--')
-    # label the mixing ratios
-    w_labels = [w*1000. if w*1000 < 1 else int(w*1000) for w in target_w]
-    for iw in range(len(target_w)):
-        ax.text(my_w[np.where(w_p == 850)[0][0], iw], 850., str(w_labels[iw]), color = 'purple')
+    # specific humidities
+    ax.semilogy(my_q, q_p, color = 'purple', lw = 0.5, ls = '--')
+    # label the specific humidities
+    q_labels = [q*1000. if q*1000 < 1 else int(q*1000) for q in target_q]
+    for iq in range(len(target_q)):
+        ax.text(my_q[np.where(q_p == 850.)[0][0], iq], 850., str(q_labels[iq]), color = 'purple')
     
     ## plot the sounding ##
     # make sure our input are lists
