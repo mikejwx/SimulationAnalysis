f9f839207fc1fe798f8c001c7bf8c911d340cc71
mikejwx <mcjohnston2803@gmail.com>
2019-01-28 22:50:05 +0000

    Fixed winds and titles

--- downwind_soundings.py
+++ downwind_soundings.py
@@ -20,7 +20,7 @@ def get_soundings(it):
     pres_cs_soundings = bilinear_interpolation(X, Y, fluxes_nc.variables[pres_key][it,:,:,:]*1., x_cs[i_cs], y_cs[i_cs], kind = 3)
     q_cs_soundings    = bilinear_interpolation(X, Y, bouy_nc.variables[q_key][it,:,:,:]*1., x_cs[i_cs], y_cs[i_cs], kind = 3)
     u_cs_soundings    = bilinear_interpolation(X, Y, wind_nc.variables[u_key][it,:,:,:]*1., x_cs[i_cs], y_cs[i_cs], kind = 3)
-    v_cs_soundings    = bilinear_interpoaltion(X, Y, wind_nc.variables[v_key][it,:,:,:]*1., x_cs[i_cs], y_cs[i_cs], kind = 3)
+    v_cs_soundings    = bilinear_interpolation(X, Y, wind_nc.variables[v_key][it,:,:,:]*1., x_cs[i_cs], y_cs[i_cs], kind = 3)
     dew_cs_soundings  = getDew(q_cs_soundings, pres_cs_soundings/100.)
 
     # Ensure our soundings directory exists, if not create it
@@ -32,7 +32,7 @@ def get_soundings(it):
         if not os.path.isdir('../Soundings/Radius_'+"{0:02d}".format(int(R[i_cs[i]]/1000.))+'/'):
             os.mkdir('../Soundings/Radius_'+"{0:02d}".format(int(R[i_cs[i]]/1000.))+'/')
         
-        plotSkewT(temp_cs_soundings[:-1,i]-273.15, dew_cs_soundings[:-1,i]-273.15, pres_cs_soundings[:-1,i]/100., u, v, my_title = 'T+' + str(int(times[it])) + 'mins, ' + str(int(R[i_cs[i]]/1000.)) + 'km downwind', CAPE = True)
+        plotSkewT(temp_cs_soundings[:-1,i]-273.15, dew_cs_soundings[:-1,i]-273.15, pres_cs_soundings[:-1,i]/100., u_cs_soundings[:-1,i], v_cs_soundings[:-1,i], my_title = 'T+' + str(int(times[it])) + 'mins, ' + str(int(R[i_cs[i]]/1000.)) + 'km downwind', CAPE = True)
         plt.savefig('../Soundings/Radius_'+"{0:02d}".format(int(R[i_cs[i]]/1000.))+'/AlongWind_sounding_T'+"{0:04d}".format(int(times[it]))+'_R'+"{0:02d}".format(int(R[i_cs[i]]/1000.))+'.png', dpi = 100)
         plt.close('all')
         print "{0:02d}".format(int(R[i_cs[i]]/1000.)) + 'km Sounding Complete.'
