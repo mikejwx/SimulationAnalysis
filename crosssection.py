e0ba2d04c61043c71163a69f2d09c1132b98e3e9
mikejwx <mcjohnston2803@gmail.com>
2019-02-01 09:06:46 +0000

    edits to the crossflow calculations

--- crosssection.py
+++ crosssection.py
@@ -8,35 +8,15 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+#plt.switch_backend('agg')
 from netCDF4 import Dataset
 from datetime import datetime as dt
 from scipy import interpolate, integrate
 from SkewT_archer import *
-from analysis_tools import bilinear_interpolation, zi, find_h
+from analysis_tools import bilinear_interpolation, get_cs_coords
 
-def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 80, fill = '='):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-      iteration - Required : current iteration (Int)
-      total     - Required : total iterations (Int)
-      prefix    - Optional : prefix string (Str)
-      suffix    - Optional : suffix string (Str)
-      decimals  - Optional : positive number of decimals in percent complete (Int)
-      length    - Optional : character length of bar (Int)
-      fill      - Optional : bar fill character (Str)
-    """
-    from __future__ import print_function
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r['+dt.now().strftime("%H:%M:%S")+'] %s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
 
-testing = True
+testing = False
 
 f = open("output.txt", "w+")
 
@@ -76,12 +56,12 @@ z_0 = np.array([1.0000004,3.6666676,7.666668,13.000004,19.666672,27.666672,
                 9205.932,14947.828,15802.464,15802.464])
 
 # Calculate the mean wind direction in the boundary layer (lowest ~ 850 m)
-z_1 = np.arange(0., 850.1, 1.)
+z_1 = np.arange(0., 500.1, 1.)
 u_1 = interpolate.interp1d(y = u_0, x = z_0, fill_value = 'extrapolate')(z_1)
 v_1 = interpolate.interp1d(y = v_0, x = z_0, fill_value = 'extrapolate')(z_1)
 
-U_0 = integrate.trapz(y = u_1, x = z_1)/850.
-V_0 = integrate.trapz(y = v_1, x = z_1)/850.
+U_0 = integrate.trapz(y = u_1, x = z_1)/500.
+V_0 = integrate.trapz(y = v_1, x = z_1)/500.
 
 wind_speed_0 = np.sqrt(U_0**2 + V_0**2)
 # Wind speed should be ~ 9.5 m/s
@@ -123,7 +103,7 @@ y_c = 4*R_i
 
 # From this center point, draw line in either direction parallel to wind_dir_0
 # get the coordinates of this line at 100 m resolution.
-x_cs, y_cs = get_cs_coords(x_c, y_c, wind_dir_0, h = 100.0)
+x_cs, y_cs = get_cs_coords(x_c, y_c, wind_dir_0, X, Y, h = 100.0)
 
 ### Testing ###
 if testing:
@@ -146,8 +126,8 @@ if testing:
     plt.text(x_cs[-1], y_cs[-1], 'B', fontsize = 20)
     plt.title('Cross Section Path')
     plt.tight_layout()
-    plt.savefig('Cross_Section_location.png', dpi = 100)
-    plt.close('all')
+    plt.savefig('../Cross_Section_location.png', dpi = 100)
+    plt.show()
 
 #================================= Section 3 ==================================#
 #                                                                              #
@@ -170,104 +150,111 @@ f.close()
 
 def get_cs(it):
     # only need data to the height just above 3000 m
-    iz3000 = np.where(np.abs(z - 3000) == np.min(np.abs(z - 3000)))[0][0]+1
+    iz3000 = np.where(np.abs(z - 5000) == np.min(np.abs(z - 5000)))[0][0]+1
     # leave the rest of the levels = 0
-
-    f = open("output.txt", "a")
-    f.write('[' + dt.now().strftime("%H:%M:%S") + '] Interpolating to the cross section coordinates...\n')
-    f.close()
-
+    my_kind = 2
+    d = 2000.
+    print('[' + dt.now().strftime("%H:%M:%S") + '] Interpolating to the cross section coordinates...')
+    
     START = dt.now()
-    print('[' + dt.now().strftime("%H:%M:%S") + '] Starting interpolation...\n')
+    print('[' + dt.now().strftime("%H:%M:%S") + '] Starting interpolation...')
     start = dt.now()
-    theta_cs = bilinear_interpolation(X, Y, bouy_nc.variables[theta_key][it,:iz3000,:,:]*1., x_cs, y_cs, kind = 3)
+    theta_cs = bilinear_interpolation(X, Y, bouy_nc.variables[theta_key][it,:iz3000,:,:]*1., x_cs, y_cs, kind = my_kind, d = d)
     end = dt.now()
     print('Finished interpolating Theta, elapsed time: ' + str(end - start))
-
+    
     start = dt.now()
-    q_cs     = bilinear_interpolation(X, Y, bouy_nc.variables[q_key][it,:iz3000,:,:]*1., x_cs, y_cs, kind = 3)
+    q_cs     = bilinear_interpolation(X, Y, bouy_nc.variables[q_key][it,:iz3000,:,:]*1., x_cs, y_cs, kind = my_kind, d = d)
     end = dt.now()
     print('Finished interpolating q, elapsed time: ' + str(end - start))
-
+    
     start = dt.now()
-    w_cs     = bilinear_interpolation(X, Y, bouy_nc.variables[w_key][it,:iz3000,:,:]*1., x_cs, y_cs, kind = 3)
+    w_cs     = bilinear_interpolation(X, Y, bouy_nc.variables[w_key][it,:iz3000,:,:]*1., x_cs, y_cs, kind = my_kind, d = d)
     end = dt.now()
     print('Finished interpolating w, elapsed time: ' + str(end - start))
     
+    start = dt.now()
+    zi_cs    = bilinear_interpolation(X, Y, zi_nc.variables[zi_key][:], x_cs, y_cs, kind = my_kind, d = d)
+    end = dt.now()
+    print('Finished interpolating zi, elapsed time: ' + str(end - start))
+    
+    start = dt.now()
+    lcl_cs    = bilinear_interpolation(X, Y, zi_nc.variables[lcl_key][:], x_cs, y_cs, kind = my_kind, d = d)
+    end = dt.now()
+    print('Finished interpolating lcl, elapsed time: ' + str(end - start))
+    
     END = dt.now()
-    f = open('output.txt', 'a')
-    f.write('[' + dt.now().strftime("%H:%M:%S") + '] Completed interpolation in ' + str(END - START) + "...\n")
-    f.close()
-
+    print('[' + dt.now().strftime("%H:%M:%S") + '] Completed interpolation in ' + str(END - START) + "...")
+    
     #================================= Section 4 ==================================#
     #                                                                              #
     # Plot the cross section and anomalies                                         #
     #                                                                              #
     #==============================================================================#
-
+    
     #print('Starting section 4')
-
+    
     # Translate the cross section coordinates into the distance downwind of the island
-    R = - np.sign(x_c - x_cs)*np.sqrt((x_cs - x_c)**2 + (y_cs - y_c)**2)
-
+    R = np.sign(x_c - x_cs)*np.sqrt((x_cs - x_c)**2 + (y_cs - y_c)**2)
+    
     # Calculate the anomaly from the horizontal mean
     theta_cs_anom = np.zeros_like(theta_cs)
     q_cs_anom = np.zeros_like(q_cs)
-
+    
     theta_hm = np.nanmean(bouy_nc.variables[theta_key][it,:iz3000,:,:], axis = (1, 2))
     q_hm     = np.nanmean(bouy_nc.variables[q_key][it,:iz3000,:,:], axis = (1, 2))
     for i in xrange(theta_cs.shape[1]):
         theta_cs_anom[:,i] = theta_cs[:,i] - theta_hm
         q_cs_anom[:,i]     = q_cs[:,i] - q_hm
-
-    # Calculate the boundary layer height
-    theta_v_cs = theta_cs*(1.0 + 0.608*q_cs)
-    zi_cs = zi(theta_v_cs, z[:iz3000])
-
+    
     # Plotting params
     text_pos_x = -R_i/1000. + R_i*0.1/1000.
     text_pos_y = 100.
     fig_width = 10 # inches
-
+    
     fig = plt.figure()
     fig.set_size_inches(fig_width, fig_width/1.5)
     ax = plt.subplot(311)
-    plt.contourf(R/1000., z[:iz3000], theta_cs_anom, cmap = 'bwr', levels = np.arange(-1.0, 1.01, 0.2), extend = 'both')
+    plt.contourf(R/1000., z[:iz3000]/1000., theta_cs_anom, cmap = 'bwr', levels = [l for l in np.linspace(-1., 1., 11) if l != 0.], extend = 'both')
     plt.colorbar(label = r'$\theta$ Anomaly (K)')
     plt.title('Potential Temperature Anomaly (K) Cross Section')
-    plt.ylim([0, 3000])
+    plt.ylim([0, 5])
     r_i = np.array([-R_i, R_i])/1000.
     plt.plot(r_i, [0, 0], 'k', lw = 5)
     plt.text(text_pos_x, text_pos_y, 'Island')
-    plt.plot(R/1000., zi_cs, 'k', lw = 2)
-    plt.ylabel('Height (m)')
+    plt.plot(R/1000., zi_cs[it,:]/1000., 'k', lw = 2)
+    plt.plot(R/1000., lcl_cs[it,:]/1000., 'grey', lw = 2)
+    plt.ylabel('Height (km)')
     plt.setp(ax.get_xticklabels(), visible=False)
-
+    
     ax1 = plt.subplot(312, sharex = ax)
-    plt.contourf(R/1000., z[:iz3000], q_cs_anom*1000., cmap = 'BrBG', levels = np.arange(-2.0, 2.01, 0.2), extend = 'both')
+    plt.contourf(R/1000., z[:iz3000]/1000., q_cs_anom*1000., cmap = 'BrBG', levels = [l for l in np.linspace(-2., 2., 11) if l != 0.], extend = 'both')
     plt.colorbar(label = r'q Anomaly (g kg$^{-1}$)')
     plt.title('Specific Humidity Anomaly (g kg$^{-1}$) Cross Section')
-    plt.ylim([0, 3000])
+    plt.ylim([0, 5])
     plt.plot(r_i, [0, 0], 'k', lw = 5)
     plt.text(text_pos_x, text_pos_y, 'Island')
-    plt.plot(R/1000., zi_cs, 'k', lw = 2)
-    plt.ylabel('Height (m)')
+    plt.plot(R/1000., zi_cs[it,:]/1000., 'k', lw = 2)
+    plt.plot(R/1000., lcl_cs[it,:]/1000., 'grey', lw = 2)
+    plt.ylabel('Height (km)')
     plt.setp(ax1.get_xticklabels(), visible=False)
-
+    
     plt.subplot(313, sharex = ax)
-    plt.contourf(R/1000., z[:iz3000], w_cs, cmap = 'bwr', levels = np.arange(-2.0, 2.01, 0.2), extend = 'both')
+    plt.contourf(R/1000., z[:iz3000]/1000., w_cs, cmap = 'bwr', levels = [l for l in np.linspace(-2.5, 2.5, 11) if l != 0.], extend = 'both')
     plt.colorbar(label = r'w (m s$^{-1}$)')
     plt.title('Vertical Velocity (m s$^{-1}$) Cross Section')
-    plt.ylim([0, 3000])
+    plt.ylim([0, 5])
     plt.plot(r_i, [0, 0], 'k', lw = 5)
     plt.text(text_pos_x, text_pos_y, 'Island')
-    plt.plot(R/1000., zi_cs, 'k', lw = 2)
-    plt.ylabel('Height (m)')
+    plt.plot(R/1000., zi_cs[it,:]/1000., 'k', lw = 2)
+    plt.plot(R/1000., lcl_cs[it,:]/1000., 'grey', lw = 2)
+    plt.ylabel('Height (km)')
     plt.xlabel('Distance from Island mid-point (km)')
     plt.suptitle('Along-wind Cross Section, at T+'+str(int(times[it])) + 'mins', fontsize = 15)
     plt.tight_layout()
     fig.subplots_adjust(top=0.88)
-    plt.savefig('AlongWind_smoothed_T'+"{0:04d}".format(int(times[it]))+'.png', dpi = 100)
+    plt.savefig('../AlongWind_interpolated_T'+"{0:04d}".format(int(times[it]))+'.png', dpi = 100)
+    plt.close('all')
     #plt.show()
     
     #================================= Section 5 ==================================#
@@ -280,44 +267,12 @@ def get_cs(it):
     # e.g. 20 km
     r_targets = [10000.0, 20000.0, 40000.0, 60000.0]
     for r_target in r_targets:
-        # Find where the radial distance from the island is nearest 25 km, this will do for now
+        # Find where the distance to the island is nearest to the target distance
         R_pos = np.where((R > 0), R, np.nan)
         iR = np.where(np.abs(R_pos - r_target) == np.nanmin(np.abs(R_pos - r_target)))[0][0]
-
-        # From this coordinate, extend a line perpendicular to the previous cross section
-        # get the coordinates of this line at h m resolution.
-        h  = 100.0
-        dx = h*np.sin(np.pi*(wind_dir_0+90.)/180.0)
-        dy = h*np.cos(np.pi*(wind_dir_0+90.)/180.0)
-
-        x_cs2 = [x_cs[iR]]
-        y_cs2 = [y_cs[iR]]
-
-        # Populate my list of x and y coordinates along the cross section
-        while (x_cs2[-1] < np.max(x))*(y_cs2[-1] < np.max(y)):
-            x_cs2.append(x_cs2[-1] + dx)
-            y_cs2.append(y_cs2[-1] + dy)
-
-        x_cs2 = x_cs2[::-1]
-        y_cs2 = y_cs2[::-1]
-
-        while (x_cs2[-1] > np.min(x))*(y_cs2[-1] > np.min(y)):
-            x_cs2.append(x_cs2[-1] - dx)
-            y_cs2.append(y_cs2[-1] - dy)
-
-        # check that all the coordinates along the cross section are within the domain
-        i = 0
-        while i < len(x_cs2):
-            if (x_cs2[i] > np.max(x)) or (x_cs2[i] < np.min(x)) or (y_cs2[i] > np.max(y)) or (y_cs2[i] < np.min(y)):
-                del x_cs2[i]
-                del y_cs2[i]
-            else:
-                i += 1
-
-        # store to arrays
-        x_cs2 = np.array(x_cs2)
-        y_cs2 = np.array(y_cs2)
-
+        
+        x_cs2, y_cs2 = get_cs_coords(x_cs[iR], y_cs[iR], wind_dir_0+90., X, Y, h = 100.0)
+        
         ### Testing ###
         if testing:
             fig = plt.figure()
@@ -335,14 +290,8 @@ def get_cs(it):
             plt.text(x_cs2[-1], y_cs2[-1], 'D', fontsize = 20)
             plt.title('Cross Section Path')
             plt.tight_layout()
-            plt.savefig('Cross_Section_location2.png', dpi = 100)
-            #plt.show()
-            plt.close('all')
-
-        # Initialise arrays for our cross section
-        theta_cs2 = np.zeros((len(z), len(x_cs2)))
-        q_cs2 = np.zeros_like(theta_cs2)
-        w_cs2 = np.zeros_like(theta_cs2)
+            plt.savefig('../Cross_Section_location2.png', dpi = 100)
+            plt.show()
 
         f = open("output.txt", "a")
         f.write('[' + dt.now().strftime("%H:%M:%S") + '] Starting interpolation...\n')
@@ -351,21 +300,33 @@ def get_cs(it):
 
         # Interpolate to cs2
         start = dt.now()
-        theta_cs2 = bilinear_interpolation(X, Y, bouy_nc.variables[theta_key][it,:iz3000,:,:], x_cs2, y_cs2, kind = 3)
+        theta_cs2 = bilinear_interpolation(X, Y, bouy_nc.variables[theta_key][it,:iz3000,:,:], x_cs2, y_cs2, kind = my_kind, d = d)
         end = dt.now()
         print('Finished interpolating Theta, elapsed time: ' + str(end - start))
 
         start = dt.now()
-        q_cs2     = bilinear_interpolation(X, Y, bouy_nc.variables[q_key][it,:iz3000,:,:], x_cs2, y_cs2, kind = 3)
+        q_cs2     = bilinear_interpolation(X, Y, bouy_nc.variables[q_key][it,:iz3000,:,:], x_cs2, y_cs2, kind = my_kind, d = d)
         end = dt.now()
         print('Finished interpolating q, elapsed time: ' + str(end - start))
 
         start = dt.now()
-        w_cs2     = bilinear_interpolation(X, Y, bouy_nc.variables[w_key][it,:iz3000,:,:], x_cs2, y_cs2, kind = 3)
+        w_cs2     = bilinear_interpolation(X, Y, bouy_nc.variables[w_key][it,:iz3000,:,:], x_cs2, y_cs2, kind = my_kind, d = d)
         end = dt.now()
         print('Finished interpolating w, elapsed time: ' + str(end - start))
         END = dt.now()
-
+        
+        start = dt.now()
+        zi_cs2     = bilinear_interpolation(X, Y, zi_nc.variables[zi_key][:], x_cs2, y_cs2, kind = my_kind, d = d)
+        end = dt.now()
+        print('Finished interpolating zi, elapsed time: ' + str(end - start))
+        END = dt.now()
+        
+        start = dt.now()
+        lcl_cs2     = bilinear_interpolation(X, Y, zi_nc.variables[lcl_key][:], x_cs2, y_cs2, kind = my_kind, d = d)
+        end = dt.now()
+        print('Finished interpolating lcl, elapsed time: ' + str(end - start))
+        END = dt.now()
+        
         f = open('output.txt', 'a')
         f.write('[' + dt.now().strftime("%H:%M:%S") + '] Completed interpolation in ' + str(END - START) + "...\n")
         f.close()
@@ -381,64 +342,68 @@ def get_cs(it):
             theta_cs2_anom[:,i] = theta_cs2[:,i] - theta_hm
             q_cs2_anom[:,i]     = q_cs2[:,i] - q_hm
 
-        # Calculate the boundary layer height
-        theta_v_cs2 = theta_cs2*(1.0 + 0.608*q_cs2)
-        zi_cs2 = zi(theta_v_cs2, z[:iz3000])
-
         # Plotting params
         text_pos_x = -R_i/1000. + R_i*0.1/1000.
         text_pos_y = 100.
         fig = plt.figure()
-
+        
         ax = plt.subplot(311)
-        plt.contourf(R2/1000., z[:iz3000], theta_cs2_anom, cmap = 'bwr', levels = np.arange(-1.0, 1.01, 0.2), extend = 'both')
+        plt.contourf(R2/1000., z[:iz3000]/1000., theta_cs2_anom, cmap = 'bwr', levels = [l for l in np.linspace(-1., 1., 11) if l != 0.], extend = 'both')
         plt.colorbar()
         plt.title('Potential Temperature Anomaly (K) Cross Section')
-        plt.ylim([0, 3000])
+        plt.ylim([0, 5])
         r_i = np.array([-R_i, R_i])/1000.
         plt.plot(r_i, [0, 0], 'k', lw = 10)
         plt.text(text_pos_x, text_pos_y, 'Island')
-        plt.plot(R2/1000., zi_cs2, 'k', lw = 2)
+        plt.plot(R2/1000., zi_cs2[it,:]/1000., 'k', lw = 2)
+        plt.plot(R2/1000., lcl_cs2[it,:]/1000., 'grey', lw = 2)
         plt.ylabel('Height (m)')
         plt.setp(ax.get_xticklabels(), visible=False)
-
+        
         ax1 = plt.subplot(312, sharex = ax)
-        plt.contourf(R2/1000., z[:iz3000], q_cs2_anom*1000., cmap = 'BrBG', levels = np.arange(-2.0, 2.01, 0.2), extend = 'both')
+        plt.contourf(R2/1000., z[:iz3000]/1000., q_cs2_anom*1000., cmap = 'BrBG', levels = [l for l in np.linspace(-2., 2., 11) if l != 0.], extend = 'both')
         plt.colorbar()
         plt.title('Specific Humidity Anomaly (g kg$^{-1}$) Cross Section')
-        plt.ylim([0, 3000])
+        plt.ylim([0, 5])
         plt.plot(r_i, [0, 0], 'k', lw = 10)
         plt.text(text_pos_x, text_pos_y, 'Island')
-        plt.plot(R2/1000., zi_cs2, 'k', lw = 2)
+        plt.plot(R2/1000., zi_cs2[it,:]/1000., 'k', lw = 2)
+        plt.plot(R2/1000., lcl_cs2[it,:]/1000., 'grey', lw = 2)
         plt.ylabel('Height (m)')
         plt.setp(ax1.get_xticklabels(), visible=False)
-
+        
         plt.subplot(313, sharex = ax)
-        plt.contourf(R2/1000., z[:iz3000], w_cs2, cmap = 'bwr', levels = np.arange(-2.0, 2.01, 0.2), extend = 'both')
+        plt.contourf(R2/1000., z[:iz3000]/1000., w_cs2, cmap = 'bwr', levels = [l for l in np.linspace(-2.5, 2.5, 11) if l != 0.], extend = 'both')
         plt.colorbar()
         plt.title('Vertical Velocity (m s$^{-1}$) Cross Section')
-        plt.ylim([0, 3000])
+        plt.ylim([0, 5])
         plt.plot(r_i, [0, 0], 'k', lw = 10)
         plt.text(text_pos_x, text_pos_y, 'Island')
-        plt.plot(R2/1000., zi_cs2, 'k', lw = 2)
+        plt.plot(R2/1000., zi_cs2[it,:]/1000., 'k', lw = 2)
+        plt.plot(R2/1000., lcl_cs2[it,:]/1000., 'grey', lw = 2)
         plt.ylabel('Height (m)')
         plt.xlabel('Distance from Island mid-point (km)')
         plt.suptitle('Cross-wind Cross Section ('+str(int(r_target/1000.))+'km downwind), at T+'+str(int(times[it])) + 'mins', fontsize = 15)
         plt.tight_layout()
         fig.subplots_adjust(top=0.88)
-        plt.savefig('CrossWind_smoothed_'+str(int(r_target/1000.))+'km_T'+"{0:04d}".format(int(times[it]))+'.png', dpi = 100)
+        plt.savefig('../CrossWind_smoothed_'+str(int(r_target/1000.))+'km_T'+"{0:04d}".format(int(times[it]))+'.png', dpi = 100)
+        plt.close('all')
         #plt.show()
 
-hours = ["{0:02d}".format(h) for h in xrange(0, 24,3)]
+#hours = ["{0:02d}".format(h) for h in xrange(0, 24,3)]
+hours = ['06', '09']
 for hour in hours:
     # e.g. the v-component winds
     bouy_nc   = Dataset('../bouy_' + hour + '.nc', 'r')
     fluxes_nc = Dataset('../fluxes_' + hour + '.nc', 'r')
+    zi_nc     = Dataset('../zi_' + hour + '.nc', 'r')
     theta_key = u'STASH_m01s00i004' #bouy.nc
     temp_key  = u'STASH_m01s16i004' #bouy.nc
     pres_key  = u'STASH_m01s00i408' #fluxes.nc
     q_key     = u'STASH_m01s00i010' #bouy.nc
     w_key     = u'STASH_m01s00i150' #bouy.nc
+    zi_key    = u'new boundary layer depth' #zi.nc
+    lcl_key   = u'lifting condensation level' #zi.nc
     z         = bouy_nc.variables[u'thlev_zsea_theta'][:]*1.
     times     = bouy_nc.variables[u'min10_0'][:]*1.
 
@@ -448,3 +413,7 @@ for hour in hours:
 
     bouy_nc.close()
     fluxes_nc.close()
+    zi_nc.close()
+
+
+
