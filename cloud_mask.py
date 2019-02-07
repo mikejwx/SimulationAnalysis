84813450cf26e93f53938f5acd489599d067315a
mikejwx <mcjohnston2803@gmail.com>
2019-02-07 12:28:32 +0000

    changed color scale

--- cloud_mask.py
+++ cloud_mask.py
@@ -3,7 +3,8 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib as mpl
-
+from multiprocessing import Pool
+import os
 """
 Code to plot cloud masks for my cloud trail simulation
 """
@@ -12,7 +13,7 @@ lwp_key = u'STASH_m01s30i405'
 
 # Read the netCDF
 print 'Reading...'
-lwp = Dataset('lwp_00.nc', 'r')
+lwp = Dataset('../lwp_00.nc', 'r')
 lsm = Dataset('/work/n02/n02/xb899100/island_masks/lsm50.nc', 'r')
 
 lwp_data = lwp.variables[lwp_key][:]*1000.
@@ -26,8 +27,8 @@ X = np.arange(0., 116000., 100.)/1000.
 Y = np.arange(0., 31900., 100.)/1000.
 X, Y = np.meshgrid(X, Y)
 
-albedo = np.where((lwp_data >= 5.), 20.5*np.log(lwp_data)-17.7, 5.)
-albedo = np.where((albedo >= 80.), 80., albedo)/100.
+#albedo = np.where((lwp_data >= 5.), 20.5*np.log(lwp_data)-17.7, 5.)
+#albedo = np.where((albedo >= 80.), 80., albedo)/100.
 
 ### Create synthetic surface flux diurnal cycle plot data ###
 t0 = 720.
@@ -36,39 +37,46 @@ dt2 = 720./2.
 H = 250.*np.cos(0.5*np.pi*(t0 - t)/dt2)**1.5
 E = 250.*np.cos(0.5*np.pi*(t0 - t)/dt2)**1.3
 
-
-for it in xrange(times.shape[0]):
+my_levels = [5., 10., 20., 50., 100., 200., 300., 400., 500., 600.]
+my_cmap = mpl.cm.get_cmap('Greys')
+def get_cm(it):
     print 'Plotting T+' + "{0:04d}".format(int(times[it])) + ' mins'
     fig = plt.figure(figsize = (18, 6))
-    gs = mpl.gridspec.GridSpec(nrows = 1, ncols = 2, height_ratios = [1], width_ratios = [2,1])
+    gs = mpl.gridspec.GridSpec(nrows = 1, ncols = 2, height_ratios = [1], width_ratios = [3,1])
     ax = fig.add_subplot(gs[0], adjustable = 'box', aspect = 'equal')
     #top left
-    ax.contourf(X-np.max(X), Y+np.max(Y), albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    ax.contourf(X-np.max(X), Y+np.max(Y), lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X-np.max(X), Y+np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #top centre
-    ax.contourf(X, Y+np.max(Y), albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    ax.contourf(X, Y+np.max(Y), lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X, Y+np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #top right
-    ax.contourf(X+np.max(X), Y+np.max(Y), albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
-    ax.contour(X+np.max(X), Y+np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'], lw = [5])
+    ax.contourf(X+np.max(X), Y+np.max(Y), lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X+np.max(X), Y+np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #centre left
-    ax.contourf(X-np.max(X), Y, albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    ax.contourf(X-np.max(X), Y, lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X-np.max(X), Y, lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #centre centre
-    ax.contourf(X, Y, albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
-    ax.contour(X, Y, lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'], lw = [5])
+    ax.contourf(X, Y, lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X, Y, lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #centre right
-    ax.contourf(X+np.max(X), Y, albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    ax.contourf(X+np.max(X), Y, lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X+np.max(X), Y, lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #bottom left
-    ax.contourf(X-np.max(X), Y-np.max(Y), albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    ax.contourf(X-np.max(X), Y-np.max(Y), lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
     ax.contour(X-np.max(X), Y-np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #bottom centre
-    alb = ax.contourf(X, Y-np.max(Y), albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    alb = ax.contourf(X, Y-np.max(Y), lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
     axins = inset_axes(ax, width = "50%", height = "5%", loc = 8, bbox_to_anchor = (0.0, -0.26, 1, 1), bbox_transform = ax.transAxes, borderpad = 0.)
-    plt.colorbar(alb, cax = axins, orientation = 'horizontal', label = 'synthetic albedo')
+    plt.colorbar(alb, cax = axins, orientation = 'horizontal', label = r'LWP (g m$^{-2}$)')
+    ax.contour(X, Y-np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     #bottom right
-    ax.contourf(X+np.max(X), Y-np.max(Y), albedo[it,:,:], cmap = 'Greys_r', levels = np.linspace(0., 1., 21))
+    ax.contourf(X+np.max(X), Y-np.max(Y), lwp_data[it,:,:], colors = my_cmap(np.arange(9)/8.), levels = my_levels, extend = 'max')
+    ax.contour(X+np.max(X), Y-np.max(Y), lsm_data[0,0,:,:], levels = [1e-08], colors = ['r'])
     ax.set_xlabel('x (km)')
     ax.set_ylabel('y (km)')
     ax.plot([0, 0, np.max(X), np.max(X), 0], [0, np.max(Y), np.max(Y), 0, 0], 'r--')
-    ax.set_title("{0:04d}".format(int(times[it]))+'mins')
+    ax.set_title('Liquid Water Path at T+' + "{0:04d}".format(int(times[it]))+' mins')
 
     ax = fig.add_subplot(gs[1], adjustable = 'box', aspect = 1)
     ax.plot(t, H, 'r', lw = 2, label = 'Sensible HF')
@@ -80,9 +88,14 @@ for it in xrange(times.shape[0]):
     ax.set_ylabel('Land-Surface Heat Flux (W m$^{-2}$)')
     lgd=plt.legend(loc='upper left', bbox_to_anchor=(1.0, 1.05), prop={'size': 12})
     fig.subplots_adjust(left = 0.0625, right = 0.87, bottom = 0.1, top = 0.9, wspace = 0.21, hspace = 0.31)
-    plt.savefig('./cloudmasks/'+"{0:04d}".format(int(times[it]))+'.png', dpi = 100, bbox_extra_artists=(lgd,))
+    plt.savefig('../cloudmasks/'+"{0:04d}".format(int(times[it]))+'.png', dpi = 100, bbox_extra_artists=(lgd,))
     if it == 1:
         plt.show()
     plt.close()
 
+p = Pool()
+p.map(get_cm, xrange(len(times)))
+p.close()
 
+command = 'convert -delay 30 -loop 0 ../cloudmasks/*.png ../cloudmasks/cm_anim.gif'
+os.system(command)
