093ead13c5d27377b2150288ac9baf554e495540
mikejwx <mcjohnston2803@gmail.com>
2019-04-18 16:54:10 +0100

    script containing keys for the netCDF

---
+++ STASH_keys.py
@@ -0,0 +1,10 @@
+"""
+Script containing all of the nc keys for variables
+"""
+w_key = u'STASH_m01s00i150'
+s_key = u'Flow-Parallel'
+n_key = u'Flow-Perpendicular'
+u_key = u'STASH_m01s00i002'
+v_key = u'STASH_m01s00i003'
+zi_new_key = u'new boundary layer depth'
+
