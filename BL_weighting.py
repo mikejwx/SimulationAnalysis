8dd69bc6a3d96422ea5c401a782d6a0fc266fa88
mikejwx <mcjohnston2803@gmail.com>
2019-04-28 18:35:40 +0100

    Started scripts to get the diagnostics for BL and ls cloud schemes.

---
+++ BL_weighting.py
@@ -0,0 +1,7 @@
+"""
+Code to estimate the profile of blending coefficient for the boundary layer
+scheme used in our simulations.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
---
+++ RH_crit.py
@@ -0,0 +1,6 @@
+"""
+Code to estimate the RH_crit profile that would be diagnosed in our simulations.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
