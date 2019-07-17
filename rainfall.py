251096445d1df2e7ddb0154d1876acc91dbccb26
mikejwx <mcjohnston2803@gmail.com>
2019-07-17 15:40:00 +0100

    Changes to plot resolution comparison rainfall during the first half of heating period.

--- rainfall.py
+++ rainfall.py
@@ -1,28 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
+from STASH_keys import ls_rain_amt_key as rain_key
+base_path = '/nerc/n02/n02/xb899100/CloudTrail/'
+paths = ['Control_short/', 'Control_0800m/', 'Control_1600m/']
 
-hours = ["{0:02d}".format(h) for h in xrange(0, 24, 3)]
+lsm_base = '/work/n02/n02/xb899100/island_masks/'
+lsm = [Dataset(lsm_base + 'lsm50.nc', 'r').variables['lsm'][:], Dataset(lsm_base + 'lsm50_0800m.nc', 'r').variables['lsm'][:], Dataset(lsm_base + 'lsm50_1600m.nc', 'r').variables['lsm'][:]]
 
-rain_key = u'STASH_m01s00i394'
-rho_key = u'STASH_m01s00i389'
-for hour in hours:
-    mr_nc = Dataset('../mr_'+ hour + '.nc', 'r')
-    fluxes_nc = Dataset('../fluxes_' + hour + '.nc', 'r')
-    z = mr_nc.variables['thlev_zsea_theta'][:]*1.
-    # to convert surface rain water mixing ratio to accumulated rain depth:
-    # (rainwater mixing ratio)*(dry air density)*(depth of grid box)*(600 second time step)*(1000 millimeters in a meter)/(1000 kg of water per unit volume)
-    # Then sum that quantity over the simulation duration
-    # N.B. z[0] 0m
-    if hour == '00':
-        rain_accum = np.sum(z[1]*fluxes_nc.variables[rho_key][1:,0,:,:]*mr_nc.variables[rain_key][1:,0,:,:]*600., axis = 0)
-    else:
-        rain_accum += np.sum(z[1]*fluxes_nc.variables[rho_key][:,0,:,:]*mr_nc.variables[rain_key][:,0,:,:]*600., axis = 0)
-    mr_nc.close()
-    fluxes_nc.close()
+res = [0.1, 0.8, 1.6]
+my_levels = [0.01, 0.5, 1, 2, 4, 8, 16, 32, 64]
+my_colors = ['blue', 'cornflowerblue', 'olive', 'gold', 'orange', 'red', 'magenta', 'aliceblue']
+target_time0 = 120.0
+target_time1 = 480.0
 
-plt.contourf(rain_accum)
-plt.colorbar()
+fig = plt.figure(tight_layout = True, figsize = (9,9))
+axa = fig.add_subplot(3, 1, 1, adjustable = 'box', aspect = 1)
+axb = fig.add_subplot(3, 1, 2, adjustable = 'box', aspect = 1)
+axc = fig.add_subplot(3, 1, 3, adjustable = 'box', aspect = 1)
+ax = [axa, axb, axc]
+cb = []
+
+for path in paths:
+    i = paths.index(path)
+    ax[i].set_ylabel('y (km)')
+    path = base_path + path
+    lwp_nc = Dataset(path + '/lwp_00.nc', 'r')
+    # get the index for our target time
+    time_key = [tkey for tkey in lwp_nc.variables.keys() if 'accum' in tkey][0]
+    time_test0 = np.abs(lwp_nc.variables[time_key][:] - target_time0)
+    time_test1 = np.abs(lwp_nc.variables[time_key][:] - target_time1)
+    it0 = np.where(time_test0 == np.min(time_test0))[0][0]
+    it1 = np.where(time_test1 == np.min(time_test1))[0][0]
+    
+    # get the coordinates
+    X, Y = np.meshgrid(np.arange(lwp_nc.variables[rain_key].shape[2])*res[i], np.arange(lwp_nc.variables[rain_key].shape[1])*res[i])
+    rainfall = lwp_nc.variables[rain_key][it1,:,:] - lwp_nc.variables[rain_key][it0,:,:]
+    cb = ax[i].contourf(X, Y, rainfall, levels = my_levels, colors = my_colors)
+    CB = fig.colorbar(cb, ax = ax[i], label = 'Rainfall Accumulation (mm)')
+    CB.ax.set_yticklabels([str(level) for level in my_levels])
+    ax[i].contour(X, Y, lsm[i][0,0,:,:], colors = ['k'])
+    ax[i].set_title('Total accumulated rainfall by peak heating for dx = ' + str(res[i]) + ' km')
+    lwp_nc.close()
+
+ax[i].set_xlabel('x (km)')
+plt.savefig('../output_from_param_analysis/rainfall_comparison.png', dpi = 150)
 plt.show()
 
 
